80ec96f82169fe6b67de5b86b566d81afcd8f59f
Saradesiree <ayala.salas.sara.desiree@iescamas.es>
2024-10-07 10:17:14 +0200

    Add files via upload

---
+++ #Esto es un comentario.py
@@ -0,0 +1,11 @@
+#Esto es un comentario
+print("¿Cómo te llamas?")
+type("Hola")
+nombre =
+#Esto es un programa que suma dos números
+print("Dime un número: b")
+num1 = int (input())
+
+print("Dime otro número")
+
+dólares=euros×1.10
